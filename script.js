f105e680c34ebdb9a1fb3f3f63f6ff4e779011fe
CmEinsig <cmneiderer@gmail.com>
2023-03-27 23:02:22 -0400

    linked javascript and did framework for functions

---
+++ script.js
@@ -0,0 +1,24 @@
+var startbutton = document.getElementById("start")
+startbutton.addEventListener('click', startgame)
+
+//Function to start the game 
+function startgame {
+
+}
+
+//funtion to get next question 
+function nextquestion {
+
+}
+
+//function for question selection 
+function answerquestion {
+
+}
+
+//Function to start timer 
+function starttimer 
+
+//function for local storage and save high score 
+function savegame ();
+
