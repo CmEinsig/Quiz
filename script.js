dbdab664713b9d2481495d0c58660d5704866225
CmEinsig <cmneiderer@gmail.com>
2023-03-27 23:55:03 -0400

    got first question placeholder to show

--- script.js
+++ script.js
@@ -3,21 +3,23 @@ var startbutton = document.getElementById("start")
 var questioncontainer = document.getElementById("questioncont")
 var shuffle 
 var currentquestion 
+var questionelement = document.getElementById('question')
+var answersElement = document.getElementById('answer')
 
 //Function to start the game 
 startbutton.addEventListener('click', startgame)
 function startgame (){
     console.log("Game started!")
 startbutton.classList.add('hide')
-questioncont.classList.remove('hide')
 currentquestion = 0
 shuffle = questions.sort(() => Math.random() -3)
+questioncont.classList.remove('hide')
+nextquestion()
 }
-
 //variables for questions 
 var questions = [
 {
-question: 'This is a question,'
+question: 'This is a question',
 answers: [
     {text: 'answer', correct: true},
     {text: 'test', correct: false},
@@ -30,6 +32,12 @@ answers: [
 
 //funtion to get next question 
 function nextquestion (){
+showquestion(shuffle[currentquestion])
+
+}
+
+function showquestion(questions){
+    questionelement.innerText = questions.question
 
 }
 
