c30c65f686678522cfe6096c28874704e6cc5105
CmEinsig <cmneiderer@gmail.com>
2023-03-27 23:35:43 -0400

    created question variables

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //variables
 var startbutton = document.getElementById("start")
 var questioncontainer = document.getElementById("questioncont")
-
+var shuffle 
+var currentquestion 
 
 //Function to start the game 
 startbutton.addEventListener('click', startgame)
@@ -9,7 +10,23 @@ function startgame (){
     console.log("Game started!")
 startbutton.classList.add('hide')
 questioncont.classList.remove('hide')
+currentquestion = 0
+shuffle = questions.sort(() => Math.random() -3)
 }
+
+//variables for questions 
+var questions = [
+{
+question: 'This is a question,'
+answers: [
+    {text: 'answer', correct: true},
+    {text: 'test', correct: false},
+    {text: 'testing', correct: false},
+    {text: 'answers', correct: false}
+]
+    }
+]
+
 
 //funtion to get next question 
 function nextquestion (){
